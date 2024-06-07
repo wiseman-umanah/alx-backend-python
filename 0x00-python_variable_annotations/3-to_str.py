2e31b7045ecc2cd3a8b8228d12cbea409f566272
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-07 12:59:42 +0100

    Completed task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""type-annotated function to_str
+that takes a float n as argument
+and returns the string
+representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """Returns a string rep of number"""
+    return str(n)
