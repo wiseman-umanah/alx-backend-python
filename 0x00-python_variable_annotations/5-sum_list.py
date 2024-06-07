ffe2c5a714b4899e278dbcadfcd349c8a7fa3835
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-07 13:17:01 +0100

    Fixed code to pass check

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ which takes a list input_list of floats
 as argument and returns their sum as a float."""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Return sum of list"""
     return sum(input_list)
