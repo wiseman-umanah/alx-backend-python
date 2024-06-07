4e2429971184cfb1b1bb5f7ce2ab7b8fac53cb3f
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-07 12:42:59 +0100

    Started a new project

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""type-annotated function add
+that takes a float a and a float b
+as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds a and b and returns a float"""
+    return a + b
