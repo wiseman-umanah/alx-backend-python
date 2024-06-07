f6e3cdd466279c2419bdd950d0636073240741d7
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-07 12:52:24 +0100

    Fixed code to pass check

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,7 @@
 that takes a string str1 and a string str2
 as arguments and returns a concatenated string"""
 
+
 def concat(str1: str, str2: str) -> str:
     """Function to concatenate strings"""
     return str1 + str2
