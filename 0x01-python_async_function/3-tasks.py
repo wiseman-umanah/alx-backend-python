4f78d12caebe44c3ee9ad57051c959ff1d0af41a
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-10 22:23:51 +0100

    Removed asyncio function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module with function task_wait_random that
 takes an integer max_delay and returns a asyncio.Task"""
-import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
