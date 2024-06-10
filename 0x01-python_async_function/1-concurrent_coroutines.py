74bf82f644fea95414c09983b238d2b4e2f97885
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-10 21:40:01 +0100

    This should pass the checks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,15 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
-    """Function to execute the code"""
+async def wait_n(n: int, max_delay: any) -> list:
+    """Gets a list of delays from wait_random
+
+    Args:
+        n (int): The number of iterations
+        max_delay (int): Maximum number of seconds to wait. Default is 10.
+
+    Returns:
+        float: A random float between 0 and max_delay.
+    """
     result = sorted([await wait_random(max_delay) for i in range(n)])
     return result
