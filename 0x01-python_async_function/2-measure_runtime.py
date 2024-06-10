334faf3f8bb61b127a65019b21b560b49d024040
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-10 21:54:41 +0100

    Fixed code to pass check

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,7 +11,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measures the time of program execution
-    
+
     Args:
         n (int): The number of times to call wait_random.
         max_delay (int): Maximum number of seconds to wait. Default is 10.
