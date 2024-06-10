4225897ad1b0c73dcd36a0500ae1fe7f7ca1aa1d
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-10 21:35:19 +0100

    This should pass the checks

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,16 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
-    """Function to execute async command"""
+async def wait_random(max_delay: int = 10) -> float:
+    """Pause for max_delay seconds and
+    return a random float between 0 and max_delay.
+
+    Args:
+        max_delay (int): Maximum number of seconds to wait.
+        Default is 10.
+
+    Returns:
+        float: A random float between 0 and max_delay.
+    """
     await asyncio.sleep(max_delay)
     return uniform(0, max_delay)
