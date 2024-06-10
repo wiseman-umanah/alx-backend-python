117aaea1f8123416d04ea02ccf2df7f78651fdc2
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-10 20:17:19 +0100

    Added docstrings to code

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""takes in 2 int arguments (in this order): n and max_delay.
+You will spawn wait_random n times with the specified max_delay.
+wait_n should return the list of all the delays (float values).
+"""
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n, max_delay):
+    """Function to execute the code"""
     result = sorted([await wait_random(max_delay) for i in range(n)])
     return result
