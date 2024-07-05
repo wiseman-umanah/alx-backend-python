75b9d33f9cdce2f883b806e754880e4c0efeb06c
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 14:47:19 +0100

    Added a new method to class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,12 @@ class TestAccessNestedMap(unittest.TestCase):
             res (str | Dict): the result
         """
         self.assertEqual(access_nested_map(nested_map, path), res)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test for KeyError"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
