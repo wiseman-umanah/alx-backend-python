42b4eeb5c166149b13cfe8ac95190a60edf86984
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 18:59:44 +0100

    Added a new method to class for testing url

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -32,17 +32,20 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
-    class TestGetJson(unittest.TestCase):
-        @patch('utils.requests.get')
-        @parameterized.expand([
-                ("http://example.com", {"payload": True}),
-                ("http://holberton.io", {"payload": False}),
-        ])
-        def test_get_json(self, test_url, test_payload, mock_get):
-            """Test url with parameterize"""
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test url with parameterize"""
+        with patch('utils.requests.get') as mock_get:
             mock_response = Mock()
             mock_response.json.return_value = test_payload
             mock_get.return_value = mock_response
             result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
+            print(result)
             self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
+        
