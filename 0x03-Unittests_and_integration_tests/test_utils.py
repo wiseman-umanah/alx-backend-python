7eb2b2480231dc5fd03ac68dba064be14e8ca291
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 14:35:30 +0100

    Started project on unit test with parameterized

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Familiarizing with utils.py"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class to test across units    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, res):
+        """test access map
+
+        Args:
+            nested_map (dict): the dict to map
+            path (tuple): value to search
+            res (str | Dict): the result
+        """
+        self.assertEqual(access_nested_map(nested_map, path), res)
